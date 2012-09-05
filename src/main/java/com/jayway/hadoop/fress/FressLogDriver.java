657b036de55aa5656aed2b1be09c8f53b9773fb4
Johan Rask <johan.rask@jayway.com>
2012-09-05 11:03:43 +0200

    Added FressLogsWork v1

---
+++ src/main/java/com/jayway/hadoop/fress/FressLogDriver.java
@@ -0,0 +1,46 @@
+package com.jayway.hadoop.fress;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import v1.MaxTemperatureReducer;
+import v7.MaxTemperatureMapper;
+
+public class FressLogDriver extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.printf("Usage: %s [generic options] <input> <output>\n",
+                    getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        Job job = new Job(getConf(), "Fress logs");
+        job.setJarByClass(getClass());
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(FressLogMapper.class);
+        job.setCombinerClass(FressLogReducer.class);
+        job.setReducerClass(FressLogReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new FressLogDriver(), args);
+        System.exit(exitCode);
+    }
+
+}
